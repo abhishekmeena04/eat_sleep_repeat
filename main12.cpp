9295f6c116aab5fb8d23a773748dc7257748a8c4
Abhishek Meena <abhishekmeena7452@gmail.com>
2024-08-27 10:38:12 +0530

    q.8

---
+++ main12.cpp
@@ -0,0 +1,22 @@
+Q.12; Write a Program to Find the ASCII Value of a Character
+#include <iostream>
+using namespace std;
+
+int main() {
+    char character;
+
+    // Taking input from the user
+    cout << "Enter a character: ";
+    cin >> character;
+
+    // Finding and displaying the ASCII value of the character
+    int asciiValue = static_cast<int>(character);
+    cout << "The ASCII value of '" << character << "' is: " << asciiValue << endl;
+
+    return 0;
+}
+Enter a character: A
+The ASCII value of 'A' is: 65
+
+Enter a character: $
+The ASCII value of '$' is: 36
