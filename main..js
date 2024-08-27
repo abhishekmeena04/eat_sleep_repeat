07ecec8c6e22f821ae5cbb740b274fdbedb5d760
Abhishek Meena <abhishekmeena7452@gmail.com>
2024-08-27 07:11:02 +0530

    first commit

---
+++ main..js
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <limits.h> // For INT_MAX
+using namespace std;
+
+int findSmallestElement(const vector<int>& arr) {
+    int smallest = INT_MAX;
+
+    for (int num : arr) {
+        if (num < smallest) {
+            smallest = num; 
+        }
+    }
+    return smallest;
+}
+
+int main() {
+    vector<int> arr = {34, 15, 88, 2, 7, -1, 50};
+
+    int smallest = findSmallestElement(arr);
+    cout << "Smallest element in the array: " << smallest << endl;
+
+    return 0;
+}
