ebd7dc368119fa8b66c1582c448c5431d3ff3724
Abhishek Meena <abhishekmeena7452@gmail.com>
2024-08-30 09:06:14 +0530

    q.18

--- TUF/main5.cpp
+++ TUF/main5.cpp
@@ -3,38 +3,24 @@
 #include <vector>
 using namespace std;
 
-void countFrequencies(const vector<int>& arr) {
-    unordered_map<int, int> frequencyMap;
-
-    // Counting the frequency of each element
-    for (int i = 0; i < arr.size(); i++) {
-        frequencyMap[arr[i]]++;
+void frequency(vector<int>& arr){
+    unordered_map<int, int> map;
+    
+    for(int i=0; i<arr.size(); i++){
+        map[arr[i]]++;
     }
-
-    // Displaying the frequency of each element
-    cout << "Element Frequency" << endl;
-    for (auto& pair : frequencyMap) {
-        cout << pair.first << " : " << pair.second << endl;
+    
+    for(auto x: map){
+        cout<<x.first <<" " <<x.second<<endl;
     }
+    
 }
 
 int main() {
-    int n;
-
-    // Taking the size of the array as input
-    cout << "Enter the number of elements in the array: ";
-    cin >> n;
-
-    vector<int> arr(n);
-
-    // Taking the elements of the array as input
-    cout << "Enter the elements of the array: ";
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
-
-    // Counting and displaying the frequency of each element
-    countFrequencies(arr);
+    
+    vector<int> arr = {1,2,1,2};
+    frequency(arr);
 
     return 0;
+    
 }
